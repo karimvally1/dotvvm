660bcd8bae5c1b8f561f27f4b757a639b377d3d9
Karim Vally <karim.vally@outlook.com>
2020-05-11 19:00:36 +0100

    Added method and command to set name.

--- Dotvvm/Web/ViewModels/DefaultViewModel.cs
+++ Dotvvm/Web/ViewModels/DefaultViewModel.cs
@@ -2,9 +2,19 @@
 {
 	public class DefaultViewModel : MasterPageViewModel
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
         public DefaultViewModel()
         {
             Title = "Default";
+        }
+
+        public void SetName()
+        {
+            FirstName = "Karim";
+            LastName = "Vally";
         }
     }
 }

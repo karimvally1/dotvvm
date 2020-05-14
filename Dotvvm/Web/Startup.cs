bf6e180ddb60772b10990d08557e6d35aff68bd9
Karim Vally <karim.vally@outlook.com>
2020-05-14 20:04:14 +0100

    Created data access and database context with EF core.

--- Dotvvm/Web/Startup.cs
+++ Dotvvm/Web/Startup.cs
@@ -3,20 +3,33 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using DotVVM.Framework.Routing;
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDataProtection();
             services.AddAuthorization();
             services.AddWebEncoders();
             services.AddDotVVM<DotvvmStartup>();
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("dotvvm")));
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext dbContext)
         {
             var dotvvmConfiguration = app.UseDotVVM<DotvvmStartup>(env.ContentRootPath);
             dotvvmConfiguration.AssertConfigurationIsValid();
@@ -25,6 +38,9 @@ namespace Web
             {
                 FileProvider = new PhysicalFileProvider(env.WebRootPath)
             });
+
+            if (env.IsDevelopment())
+                dbContext.Database.EnsureCreated();
         }
     }
 }
